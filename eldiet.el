7b76be8bee9a68bd42a403eb0a1f1c5e2b5eb288
Schroedingberg <aaronrebmann@gmail.com>
2016-03-26 16:38:06 +1100

    Fitted the source file to the standards

--- eldiet.el
+++ eldiet.el
@@ -1,12 +1,15 @@
-;; Doc;;
+;;; package --- Summary
+;; Author:
+;; Keywords:
+;; Homepage:
+;;; Commentary:
+
+
+;;; Code:
+(require 'org)
 
 
 
+(provide 'eldiet)
+;;; eldiet.el ends here
 
-
-
-
-
-
-
-;; Code ;;
