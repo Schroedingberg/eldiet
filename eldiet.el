6c0ad3385f764f2d183703004cbed6358d60439d
Schroedingberg <aaronrebmann@gmail.com>
2016-06-25 11:26:14 +1000

    safety commit

--- eldiet.el
+++ eldiet.el
@@ -1,8 +1,12 @@
 ;;; package --- Summary
-;; Author:
-;; Keywords:
-;; Homepage:
-;;; Commentary:
+;; Author: Aaron Rebmann
+;; Keywords: Nutrition, Org-mode, Diet
+;; Homepage: https://github.com/Schroedingberg/eldiet
+;;; Commentary: A nutrition planner for Emacs, built upon org mode.
+;;; Plan so far: Use the capture functionality to capture a meal you eat.
+;;; Then for example a fuzzy search in a database (or an org table, whatever I will come up with)
+;;; could be executed...
+;;; Org dashboard could be tweaked to show how much kcal are left...
 
 
 ;;; Code:
