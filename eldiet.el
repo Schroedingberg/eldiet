2a4bbbe21785b9ee452c0078be67a477ff984ce5
Schroedingberg <aaronrebmann@gmail.com>
2016-07-08 16:07:34 +0200

    Did some (not working well) experiments at helm and org parsing

--- eldiet.el
+++ eldiet.el
@@ -2,11 +2,13 @@
 ;; Author: Aaron Rebmann
 ;; Keywords: Nutrition, Org-mode, Diet
 ;; Homepage: https://github.com/Schroedingberg/eldiet
-;;; Commentary: A nutrition planner for Emacs, built upon org mode.
+;;; Commentary:
+;;; A nutrition planner for Emacs, built upon org mode.
 ;;; Plan so far: Use the capture functionality to capture a meal you eat.
 ;;; Then for example a fuzzy search in a database (or an org table, whatever I will come up with)
 ;;; could be executed...
 ;;; Org dashboard could be tweaked to show how much kcal are left...
+;;; In modeline, kcal could as well be shown
 
 
 ;;; Code:
@@ -42,34 +44,43 @@ files were last parsed.")
   "Prompts for a food and the nutrients that are specified in
   eldiet-nutrients. It is a wrapper around eldiet add food to database.")
 
-(defun get-string-from-file (filePath nutrients)
+(defun eldiet-get-string-from-file (filePath nutrients)
   "Return filePath's file content tailored to obtain property values from the org based database."
   (with-temp-buffer
     (insert-file-contents filePath)
-    (mapcar 'org-property-values nutrients)
-    ;; The parser needs to be transformed to return alists
-    (mapcar ')))
-
-
+    (setq nutrients-values (mapcar 'org-property-values nutrients))))
+   
 (defun eldiet-parse-org-database ()
   "Parse the entries listed in eldiet-database using get-string-from-file."
-  (setq db-file (get-string-from-file eldiet-database eldiet-nutrients)))
+  (setq db-file (eldiet-get-string-from-file eldiet-database eldiet-nutrients))
+  )
 
 
 (defun eldiet-init ()
   "Check that files specified by user exist."
   (mapc (lambda (file)
 	  (unless (f-file? file)
-	    (user-error "eldiet file %s could not be found." file)))
-	(-flatten (list eldiet-database))))
+	    (user-error "eldiet file %s could not be found." file)))))
+
+(setq eldiet-example-candidates-list '(("Milk" . ("3.5" "1.5" "5.0"))))
+
+(setq helm-source-eldiet
+      '((name . "FOOD")
+	(candidates . eldiet-example-candidates-list)
+	(action . (lambda (candidate )
+		    (helm-marked-candidates)))))
 
 
 (defun eldiet-select-food ()
   "The helm dialog you can select the food for a meal from."
   (interactive)
-  (helm :name "Hello"
-	:buffer "*test*")
-  )
+  (insert
+   (mapconcat 'identity
+   (helm :sources '(helm-source-eldiet) :buffer "*helm eldiet select food*")
+   ",")))
+
+
+
 
 
 (provide 'eldiet)
